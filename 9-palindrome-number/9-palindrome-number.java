3ccb0733b7797735a15435042a1e45c609489598
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2022-09-18 11:30:33 +0530

    Time: 14 ms (65.42%), Space: 44.4 MB (68.66%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -6,7 +6,7 @@ class Solution {
       }
         else if(x < 0 || x % 10==0){
             return false;
-      }
+        }
         int rev = 0;
         while(x>rev){
             rev = rev * 10 + x % 10;
