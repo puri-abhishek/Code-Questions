b536a7190393247803086396996a82488b1b2bf8
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2022-09-18 11:48:08 +0530

    Time: 16 ms (42.65%), Space: 44.3 MB (73.38%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,18 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
      
-        if(x == 0){
-          return true;
-      }
-       if(x < 0 || x % 10==0){
-            return false;
-      }
-        int rev = 0;
-        while(x>rev){
-            rev = rev * 10 + x % 10;
-            x = x / 10;
+       int rev = 0, temp = x;
+        while(temp>0){
+            rev = rev * 10 + temp % 10;
+            temp = temp / 10;
         }
-        if(x == rev || x == rev / 10){
+        if(x == rev){
             return true;
         }
         else {
