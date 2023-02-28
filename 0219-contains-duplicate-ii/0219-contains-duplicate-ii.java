cd9ce2e01b9947450dfebac31604d84e9c568b76
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-02-28 11:54:12 +0530

    Time: 20 ms (53.85%), Space: 50.7 MB (61.08%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,14 +1,31 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         int n=nums.length;
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n && j<=i+k;j++){
+//                  if(nums[i]==nums[j]){
+//                      return true;
+//                 }
+//              }
+//         }
+//         return false;
+    
+//     }
+// }
+//------------------------------[HashMap]-----------------------------//
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         int n=nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n && j<=i+k;j++){
-                 if(nums[i]==nums[j]){
-                     return true;
+            if(map.containsKey(nums[i])){
+                if(i-map.get(nums[i])<=k){
+                    return true;
                 }
-             }
+            }
+            map.put(nums[i],i);
         }
         return false;
-    
     }
 }
