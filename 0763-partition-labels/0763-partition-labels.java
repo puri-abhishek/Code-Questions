919e3049fc073d99915acfb5531ec320ece1df8c
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-07-11 23:41:33 +0530

    Time: 9 ms (45.66%), Space: 41.6 MB (45.05%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch, i);
+        }
+        
+        List<Integer> arr = new ArrayList<>();
+        int  max = 0, prev = -1;
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            max = Math.max(max, map.get(ch));
+            if(max == i){
+                arr.add(max - prev);
+                prev = max;
+            }
+            else
+                continue;
+        }
+        return arr;
+    }
+}
