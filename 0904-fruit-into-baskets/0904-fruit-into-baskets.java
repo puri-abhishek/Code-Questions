8d3290f06fded8ded7b829cbcc299d8ac7e956c3
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-07-15 21:55:43 +0530

    Time: 62 ms (58.69%), Space: 55.4 MB (11.77%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,40 @@
+/*<time limit exceeds>
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int max = 0;
+        
+        for(int i = 0; i < fruits.length; i++){
+            Set<Integer> basket = new HashSet<>();
+            int j = i;
+            for( j = i; j < fruits.length; j++){
+                if(basket.size() == 2 && !basket.contains(fruits[j]))
+                    break;
+                basket.add(fruits[j]);
+            } 
+            max = Math.max(max, j - i);
+        }
+        return max;
+    }
+}
+*/
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int i = 0, j = 0;
+        int max = 0;
+        Map<Integer, Integer> b = new HashMap<>();
+        while (j < fruits.length) {
+            b.put(fruits[j], 1 + b.getOrDefault(fruits[j], 0));
+            while (b.size() > 2) {
+                b.put(fruits[i], b.get(fruits[i]) - 1); 
+                if (b.get(fruits[i]) == 0) {    
+                    b.remove(fruits[i]);
+                }
+                i++;   
+            }
+            max = Math.max(max, j - i + 1); 
+            j++;    
+        }
+
+        return max;  
+    }
+}
