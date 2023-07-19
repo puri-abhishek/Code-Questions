9aac998c950da55eb68e4abf83fd9b195c94f983
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-07-19 20:08:56 +0530

    Time: 2 ms (86.11%), Space: 39.5 MB (96.11%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,4 +1,5 @@
-/*class Solution {
+/*
+class Solution {
     public int arrangeCoins(int n) {
       int ans = 0, i = 1;
         while(n>0){
@@ -9,13 +10,32 @@
         }
         return ans;
     }
-}*/
+}
+*/
 
+/*
 class Solution {
     public int arrangeCoins(int n) {
         int i;
         for(i = 1; i <= n; i++)
             n = n - i;
         return i - 1;
+    }
+}
+*/
+
+class Solution {
+    public int arrangeCoins(int n) {
+        long n_d = (long) n;
+        long l = 1, r = n_d;
+        
+        while(l <= r){
+            long mid = l + (r - l) / 2;
+            if(mid * (mid + 1) / 2 <= n_d  )
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return (int) l - 1 ;
     }
 }
