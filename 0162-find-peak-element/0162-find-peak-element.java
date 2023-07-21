f5c7d2217eafdc5f9bcea691c00bbd6c084ac273
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-07-21 20:13:10 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (24.65%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int findPeakElement(int[] nums) {
          int max = nums[0];
@@ -10,5 +11,51 @@ class Solution {
             }
         }
         return index;
+    }
+}
+*/
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length, l = 0, r = n - 1 ;
+        if (n == 1) 
+            return 0;
+        if (n == 2)
+        {
+            if (nums[1] > nums[0])
+                return 1;
+            if (nums[0] > nums[1]) 
+                return 0;
+        }
+         while(l <= r)
+        {
+            int mid = l + (r - l) / 2;
+             
+            if(mid > 0 && mid < r){
+                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                    return mid;
+                else if (nums[mid] > nums[mid - 1]) 
+                    l = mid + 1;
+                else  
+                    r = mid - 1; 
+                }
+             
+            else if(mid == 0)
+            {
+            if(nums[mid] > nums[mid + 1])
+                return 0;
+            else 
+                l = mid + 1;
+            }
+             
+             else if(mid == r)
+             {
+             if(nums[mid] > nums[mid - 1])
+                 return mid;
+             else 
+                 r = mid - 1;
+            } 
+        }
+        return -1;
     }
 }
