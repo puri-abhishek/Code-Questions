c2b64b1b40d666f3160a3950473fdc9fe99325e9
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-07-21 20:14:52 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (96.02%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -31,29 +31,30 @@ class Solution {
         {
             int mid = l + (r - l) / 2;
              
-            if(mid > 0 && mid < r){
+            if(mid > 0 && mid < r)
+            {
                 if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
                     return mid;
                 else if (nums[mid] > nums[mid - 1]) 
                     l = mid + 1;
                 else  
                     r = mid - 1; 
-                }
+            }
              
             else if(mid == 0)
             {
-            if(nums[mid] > nums[mid + 1])
-                return 0;
-            else 
-                l = mid + 1;
+                if(nums[mid] > nums[mid + 1])
+                    return 0;
+                else 
+                    l = mid + 1;
             }
              
-             else if(mid == r)
-             {
-             if(nums[mid] > nums[mid - 1])
-                 return mid;
-             else 
-                 r = mid - 1;
+            else if(mid == r)
+            {
+                 if(nums[mid] > nums[mid - 1])
+                     return mid;
+                 else 
+                     r = mid - 1;
             } 
         }
         return -1;
