332a2a885b6e185c72f7bb2b46a0ec77f0d69366
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-07-26 19:58:32 +0530

    Time: 15 ms (5.29%), Space: 41.7 MB (91.06%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> arr = new ArrayList<>();
+        Arrays.sort(nums);
+        subseq(arr, new ArrayList<>(), nums, 0);
+        return arr;
+    }
+    
+    public void subseq(List<List<Integer>> arr , List<Integer> tempArr, int [] nums, int i){
+        if(arr.contains(tempArr))
+            return;
+        if(i == nums.length)
+        {
+            arr.add(new ArrayList<>(tempArr));
+            return;
+        }
+        tempArr.add(nums[i]);
+        subseq(arr, tempArr, nums, i + 1);
+        tempArr.remove(tempArr.size() - 1);
+        subseq(arr, tempArr, nums, i + 1);
+    }
+}
