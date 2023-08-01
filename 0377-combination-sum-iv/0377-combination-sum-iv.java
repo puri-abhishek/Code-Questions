9de3a13ae5fdfe5b2c7ea4082848d92c30b51462
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-08-01 21:59:40 +0530

    Time: 1 ms (70.30%), Space: 40.2 MB (34.40%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,33 @@
+/*
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if(target == 0)
+            return 1;
+        
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(target >= nums[i])
+                ans += combinationSum4(nums, target - nums[i]);
+        }
+        return ans;
+    }
+} 
+*/
+
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int dp[] = new int[target + 1];
+        dp[0] = 1;
+        for(int i = 0; i <= target; i++)
+        {
+            for(int num : nums)
+            {
+                if(num <= i)
+                {
+                  dp[i] = dp[i] + dp[i - num];  
+                }    
+            }
+        }
+        return dp[target]; 
+     }
+}
