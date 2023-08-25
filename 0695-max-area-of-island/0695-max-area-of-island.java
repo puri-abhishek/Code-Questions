a82824148f09434d2d0f35b6085062431ffe7a06
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-08-25 19:59:35 +0530

    Time: 2 ms (86.39%), Space: 43.1 MB (86.66%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -7,24 +7,21 @@ class Solution {
             {
                 if(grid[i][j] == 1)
                 {
-                    area = BFS(grid, i, j, 0);
+                    area = BFS(grid, i, j);
                     maxArea = Math.max(area, maxArea);
                 }
             }
         }
         return maxArea;
     }
-     public int BFS(int[][] grid, int i, int j, int area)
+     public int BFS(int[][] grid, int i, int j)
     {
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0)
             return 0;
         else
         {
             grid[i][j] = 0;
-            return 1 + BFS(grid, i + 1, j, area) +
-            BFS(grid, i - 1, j, area) +
-            BFS(grid, i, j + 1, area) +
-            BFS(grid, i, j - 1, area);
+            return 1 + BFS(grid, i + 1, j) + BFS(grid, i - 1, j) + BFS(grid, i, j + 1) + BFS(grid, i, j - 1);
         }
     }
 }
