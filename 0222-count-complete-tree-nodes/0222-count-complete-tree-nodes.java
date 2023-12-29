bad9dc43e91c1d8a132399a1d3a97e141a189f18
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2023-12-29 20:18:05 +0530

    Time: 0 ms (100.00%), Space: 47.8 MB (5.06%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+/* class Solution {
     int count = 0;
     public int countNodes(TreeNode root) {
         if(root == null)
@@ -25,5 +25,24 @@ class Solution {
         
         return count;
         
+    }
+}
+*/
+class Solution {
+    public int countNodes(TreeNode root) {
+        int ans=0;
+        if(root==null)
+            return 0;
+        if(root.right!=null)
+        {
+            int rnodes=countNodes(root.right);
+            ans+=rnodes;
+        }
+        if(root.left!=null)
+        {
+            int lnodes=countNodes(root.left);
+            ans+=lnodes;
+        }
+        return ans+1;
     }
 }
