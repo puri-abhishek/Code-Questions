f061b9ed7af1d3b3be7938440efcd287b8aee989
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2024-02-29 20:05:08 +0530

    Time: 19 ms (16.53%), Space: 44.5 MB (45.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
+       /* if(s.length()!=t.length())
             return false;
 
         int count[] = new int[26];
@@ -11,6 +11,34 @@ class Solution {
         for(int freq : count){
             if(freq!=0)
                 return false;
+        }
+        return true;*/
+        if(s.length()!=t.length())
+            return false;
+        
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(hm.containsKey(c)){
+                hm.put(c, hm.get(c) + 1);
+            }
+            else{
+                hm.put(c, 1);
+            }
+        }
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            if(hm.containsKey(c)){
+                if(hm.get(c) == 1){
+                    hm.remove(c);
+                }
+                else{
+                    hm.put(c, hm.get(c) - 1);
+                }
+            }
+            else{
+                return false;
+            }
         }
         return true;
     }
