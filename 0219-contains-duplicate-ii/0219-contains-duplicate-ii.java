a054e0721759ad1faa577e88be5ffaa0b10feb91
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2024-03-01 19:52:23 +0530

    Time: 19 ms (60.13%), Space: 57.5 MB (47.09%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,16 +1,3 @@
-// class Solution {
-//     public boolean containsNearbyDuplicate(int[] nums, int k) {
-//         for(int i=0;i<nums.length;i++){
-//             for(int j=i+1;j<nums.length && j<=i+k;j++){
-//                  if(nums[i]==nums[j]){
-//                      return true;
-//                 }
-//              }
-//         }
-//         return false;
-    
-//     }
-// }
 
 // ------Time limit exceeds for the below code--------
 
