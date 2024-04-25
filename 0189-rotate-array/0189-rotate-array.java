cd581b8299ead08f3917518e47079b146c7cbefb
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2024-04-25 12:19:38 +0530

    Time: 1 ms (49.04%), Space: 56.9 MB (84.59%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,19 +1,36 @@
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         k = k % nums.length;
+//         int n = nums.length;
+//         reverse(nums, 0, n - 1);
+//         reverse(nums, 0, k - 1);
+//         reverse(nums, k, n - 1);
+//     }
+    
+//     public void reverse(int []nums, int first, int last){
+//         while(first < last){
+//             int temp = nums[first];
+//             nums[first] = nums[last];
+//             nums[last] = temp;
+//             first ++;
+//             last --;
+//         }
+//     }
+// }
+
 class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
-        int n = nums.length;
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
-    }
-    
-    public void reverse(int []nums, int first, int last){
-        while(first < last){
-            int temp = nums[first];
-            nums[first] = nums[last];
-            nums[last] = temp;
-            first ++;
-            last --;
+        int[] temp = new int[nums.length];
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[nums.length - k + i];
+        }
+        for (int i = k; i < nums.length; i++) {
+            temp[i] = nums[i - k];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = temp[i];
         }
     }
 }
