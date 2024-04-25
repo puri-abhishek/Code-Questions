53fddbc7489cd30367db4c077d68841813f6ae40
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2024-04-25 12:22:08 +0530

    Time: 0 ms (100.00%), Space: 57.3 MB (40.35%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,36 +1,37 @@
-// class Solution {
-//     public void rotate(int[] nums, int k) {
-//         k = k % nums.length;
-//         int n = nums.length;
-//         reverse(nums, 0, n - 1);
-//         reverse(nums, 0, k - 1);
-//         reverse(nums, k, n - 1);
-//     }
-    
-//     public void reverse(int []nums, int first, int last){
-//         while(first < last){
-//             int temp = nums[first];
-//             nums[first] = nums[last];
-//             nums[last] = temp;
-//             first ++;
-//             last --;
-//         }
-//     }
-// }
-
 class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
-        int[] temp = new int[nums.length];
-
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[nums.length - k + i];
-        }
-        for (int i = k; i < nums.length; i++) {
-            temp[i] = nums[i - k];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = temp[i];
+        int n = nums.length;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    
+    public void reverse(int []nums, int first, int last){
+        while(first < last){
+            int temp = nums[first];
+            nums[first] = nums[last];
+            nums[last] = temp;
+            first ++;
+            last --;
         }
     }
 }
+
+// another solution that i tried
+//class Solution {
+//     public void rotate(int[] nums, int k) {
+//         k = k % nums.length;
+//         int[] temp = new int[nums.length];
+
+//         for (int i = 0; i < k; i++) {
+//             temp[i] = nums[nums.length - k + i];
+//         }
+//         for (int i = k; i < nums.length; i++) {
+//             temp[i] = nums[i - k];
+//         }
+//         for (int i = 0; i < nums.length; i++) {
+//             nums[i] = temp[i];
+//         }
+//     }
+// }
