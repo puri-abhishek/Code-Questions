5cb3e6cbe6a2c4ab5a9156549c0d514ea7e32e7b
Abhishek Puri <92423196+puri-abhishek@users.noreply.github.com>
2024-08-29 12:36:12 +0530

    Time: 5 ms (93.00%), Space: 42.5 MB (95.46%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -14,3 +14,16 @@ class Solution {
         return sb.toString().trim();
     }
 }
+
+//< this code has problem with spaces>
+// class Solution { 
+//     public String reverseWords(String s) {
+//         s = s.trim();
+//         String arr[] = s.split(" ");
+//         StringBuilder sb = new StringBuilder();
+//         for(int i = arr.length - 1; i >= 0; i--){
+//             sb.append(arr[i]).append(" ");
+//         }
+//         return sb.toString().trim();
+//     }
+// }
